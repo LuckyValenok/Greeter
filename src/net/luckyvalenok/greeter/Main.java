4fd6d4bdd40da25ef2eaad0ca7f3f96f7b6e09e8
Vladislav <luckyvalenok1@gmail.com>
2020-09-18 19:17:35 +0500

    Скоратил метод определения вис. года

--- src/net/luckyvalenok/greeter/Main.java
+++ src/net/luckyvalenok/greeter/Main.java
@@ -69,13 +69,7 @@ public class Main {
     }
 
     private static boolean isLeapYear(int year) {
-        if (year % 4 == 0)
-            if (year % 100 == 0)
-                return year % 400 == 0;
-            else
-                return true;
-        else
-            return false;
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 
     public static String format(int i, String one, String two, String three) {
